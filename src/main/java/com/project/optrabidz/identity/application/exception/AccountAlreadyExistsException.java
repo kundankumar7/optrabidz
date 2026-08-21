edfdef5383664eec69a801ed6a5c8c7fb515d150
Kumar Kundan <kumarkundan0809@icloud.com>
2026-08-21 16:21:31 +0530

    refactor: migrate identity errors (KAN-24)

--- src/main/java/com/project/optrabidz/identity/application/exception/AccountAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.project.optrabidz.identity.application.exception;
-
-public class AccountAlreadyExistsException extends RuntimeException {
-    public AccountAlreadyExistsException(String message) {
-        super(message);
-    }
-}
